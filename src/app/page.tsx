5a9b587657877b302ecc25fe0937951f8b444b57
FATİH GÜZEL <98105601+fatihguzel@users.noreply.github.com>
2023-08-12 15:19:52 +0300

    Project Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold text-center">
+          Turkish Twitter
+          <br />
+        </h1>
+      </div>
+    </main>
+  );
+}
